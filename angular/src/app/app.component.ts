75fac29699b7a54a743971b2f4d9474a3d38e251
Kovács Rudolf <kovacsrud@gszi.edu.hu>
2025-03-11 19:16:38 +0100

    posts

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { ListaComponent } from './components/lista/lista.component';
+import { PostsComponent } from './components/posts/posts.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent,ListaComponent],
+  imports: [RouterOutlet,HeaderComponent,ListaComponent,PostsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
